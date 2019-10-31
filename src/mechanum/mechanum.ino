ff3c99628b4880daf62e9512dba08a4e613cd967
Kazuryu0907 <41988990+Kazuryu0907@users.noreply.github.com>
2019-10-31 21:42:26 +0900

    ino end

--- src/mechanum/mechanum.ino
+++ src/mechanum/mechanum.ino
@@ -18,19 +18,27 @@ void setup() {
   }
   Serial.print(F("\nPS4 OK Program  Start\n"));
 }
-
-
+uint8_t StickX;
+uint8_t StickY;
+uint8_t AnalogR2;
+uint8_t AnalogL2;
+uint8_t Buttons;
+void SendPacket();
 void loop() {
   Usb.Task();
 
   if (PS4.connected()) 
   { //通信中に実行
     // put your main code here, to run repeatedly:
-    uint8_t StickX =  PS4.getAnalogHat(LeftHatY) - 127;
-    uint8_t StickY =  PS4.getAnalogHat(LeftHatX) - 127;
-    uint8_t AnalogR2 = PS4.getAnalogButton(R2);
-    uint8_t AnalogL2 = PS4.getAnalogButton(L2);
-    uint8_t Buttons = PS4.getButtonPress(TRIANGLE) << 3 || PS4.getButtonPress(CIRCLE) << 2 || PS4.getButtonPress(CROSS) << 1 || PS4.getButtonPress(SQUARE);
+    StickX =  PS4.getAnalogHat(LeftHatY);
+    StickY =  PS4.getAnalogHat(LeftHatX);
+    AnalogR2 = PS4.getAnalogButton(R2);
+    AnalogL2 = PS4.getAnalogButton(L2);
+    Buttons = PS4.getButtonPress(TRIANGLE) << 3 || PS4.getButtonPress(CIRCLE) << 2 || PS4.getButtonPress(CROSS) << 1 || PS4.getButtonPress(SQUARE);
+    if(StickX == 255)StickX = 254;
+    if(StickY == 255)StickY = 254;
+    if(AnalogR2 == 255)AnalogR2 = 254;
+    if(AnalogL2 == 255)AnalogL2 = 254;
     Serial.print("X:");
     Serial.print(StickX);
     Serial.print("Y:");
@@ -39,6 +47,10 @@ void loop() {
     Serial.print(AnalogR2);
     Serial.print("L2:");
     Serial.print(AnalogL2);
+    Serial.print("Buttons:");
+    Serial.println(Buttons,BIN);
+
+    SendPacket();
       //接続を切る
     if (PS4.getButtonClick(PS)) {
       PS4.disconnect();
@@ -46,3 +58,13 @@ void loop() {
   }
 }
 //スティック
+
+void SendPacket()
+{
+  Serial.write(255);
+  Serial.write(StickX);
+  Serial.write(StickY);
+  Serial.write(AnalogR2);
+  Serial.write(AnalogL2);
+  Serial.write(Buttons);
+}
